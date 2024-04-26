1f731df74b93ec5220be316191b9cc24d50c5c51
Maile <maile@codewithher.org>
2024-04-26 16:17:46 +0200

    setup husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen w-full flex-col items-start justify-start bg-stormy">
-      <div className="flex h-[80vh] w-full px-10 flex-col items-center justify-end bg-peacock">
+      <div className="flex h-[80vh] w-full px-10 flex-col items-start justify-end bg-peacock">
         <span className="font-sans font-bold text-lg text-orangejulius mb-6">
           a case study and celebration
         </span>
