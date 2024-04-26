6b78b2f8992fd578e49352be8026e608b5e851e6
Maile <maile@codewithher.org>
2024-04-26 22:52:01 +0200

    animating hero text

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,20 +3,22 @@ import { Heading } from '..';
 
 const Hero = () => {
   return (
-    <section className="flex h-[90vh] w-full px-10 flex-col items-center justify-center bg-peacock">
+    <section className="flex h-[90vh] w-full px-5 md:px-10 flex-col items-center justify-center bg-peacock">
       <div className="flex flex-col items-center justify-center">
-        <div className="flex w-full px-5 items-start justify-between">
-          <span className="font-sans font-bold text-xl text-orangejulius mb-6">
+        <div className="flex w-full px-2 md:px-5 items-start justify-between">
+          <span className="animate-fadeInBottom font-sans font-bold text-xl md:text-3xl text-orangejulius mb-6">
             a case study
           </span>
-          <span className="font-sans font-bold text-xl text-orangejulius mb-6">
+          <span className="animate-fadeInBottom font-sans font-bold text-xl md:text-3xl text-orangejulius mb-6">
             and
           </span>
-          <span className="font-sans font-bold text-xl text-orangejulius mb-6">
+          <span className="animate-fadeInBottom font-sans font-bold text-xl md:text-3xl text-orangejulius mb-6">
             celebration
           </span>
         </div>
-        <Heading level={1} text="mac &amp; cheese" />
+        <div className="animate-fadeInLeft">
+          <Heading level={1} text="mac &amp; cheese" />
+        </div>
       </div>
     </section>
   );
