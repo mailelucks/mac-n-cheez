c892bfd41c5fd664dde53cd478ee615aee066627
Maile <maile@codewithher.org>
2024-04-26 16:18:26 +0200

    setup husky rules and commands

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'npm tsc --noEmit',
+  '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
 
   // Lint & Prettify TS and JS files
   '**/*.(ts|tsx|js)': (filenames) => [
-    `npm eslint ${filenames.join(' ')}`,
-    `npm prettier --write ${filenames.join(' ')}`
+    `npx eslint ${filenames.join(' ')}`,
+    `npx prettier --write ${filenames.join(' ')}`
   ],
 
   // Prettify only Markdown and JSON files
-  '**/*.(md|json)': (filenames) => `npm prettier --write ${filenames.join(' ')}`
+  '**/*.(md|json)': (filenames) => `npx prettier --write ${filenames.join(' ')}`
 };
